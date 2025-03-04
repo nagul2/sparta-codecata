47e8c3e4eb41d503af05249e57188c920e42c102
nagul2 <hayani0000@gmail.com>
2025-03-04 09:18:09 +0900

    5 - 숫자 비교하기 풀이

---
+++ src/test/numCompare.java
@@ -0,0 +1,24 @@
+package test;
+
+public class numCompare {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(0, 0));
+        System.out.println(solution.solution(2, 3));
+        System.out.println(solution.solution(1540, 1540));
+        System.out.println(solution.solution(-777, -777));
+
+    }
+
+    static class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            if (num1 != 0 || num2 != 0) {
+                return (num1 == num2) ? 1 : -1;
+            }
+            return answer;
+        }
+    }
+}
+
+
